bee4ae36144445c1e75da8d06389260f32049f28
Ruben Rodriguez <ruben.rdrgz2001@gmail.com>
2021-09-21 09:52:10 -0500

    Completed JSBasicsExercise WITH BONUS

---
+++ script.js
@@ -0,0 +1,45 @@
+// Exercise JS
+// One
+console.log(20);
+
+// Two
+// Numbers, Booleans, Strings, Null, & Underlined
+
+// Three
+// +, -. *, /, **, AND %
+
+// Four
+console.log(5%4);
+console.log(5%2);
+// console.log(1%5);
+
+// Five
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// Six
+console.log("Hawk" + 'eye');
+
+// Seven
+console.log("Ruben".length + 'Rodriguez'.length);
+
+// Eight
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+// ONE
+console.log("\\");
+
+// TWO
+console.log(5 ** 4 - 9);
+
+// THREE
+console.log(("I".length + "Am".length) * "Iron".length / "Man".length % 4);
+
+// FOUR
+console.log("CAP"[1] + "\n" + "VISION"[0] + "\n" + "OKOYE"[4] + "\n" + "NICK FURY"[0] + "\n" + "GROOT"[0] + "\n" + "DOCTOR STRANGE"[13] + "\n" + "THOR"[3] + "\n" + "STAR-LORD"[0]);
+
+// Five
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
